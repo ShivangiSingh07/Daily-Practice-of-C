3d3f23e0ff3e1320c22ad4efd7bf5225910a434f
Shivangi Singh <shivangisingh7@outlook.com>
2026-08-15 21:29:22 +0530

    Update Prob4.c

--- Prob4.c
+++ Prob4.c
@@ -8,11 +8,21 @@ target = 9
 Output:
 [0, 1]
 */
-arr = [2, 7, 11, 15]
-target = 9
+#include <stdio.h>
 
-for i in range(len(arr)):
-    for j in range(i + 1, len(arr)):
-        if arr[i] + arr[j] == target:
-            print([i, j])
-            break
+int main() {
+    int arr[] = {2, 7, 11, 15};
+    int target = 9;
+    int n = 4;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+
+            if (arr[i] + arr[j] == target) {
+                printf("[%d, %d]", i, j);
+            }
+        }
+    }
+
+    return 0;
+}
