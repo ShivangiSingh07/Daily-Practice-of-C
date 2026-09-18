921d694967cbb61012e3041076be072c5070c3f4
Shivangi Singh <shivangisingh7@outlook.com>
2026-09-18 22:41:36 +0530

    Create Rain_water.c

---
+++ Rain_water.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/*
+Question: Trapping Rain Water
+
+Given an array representing the heights of bars, find the
+total amount of rainwater that can be trapped between them.
+
+Input:  0 1 0 2 1 0 1 3 2 1 2 1
+Output: 6
+
+Use the Two Pointer technique.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+int trap(int height[], int n)
+{
+    int left = 0, right = n - 1;
+    int leftMax = 0, rightMax = 0;
+    int water = 0;
+
+    while (left < right)
+    {
+        if (height[left] < height[right])
+        {
+            if (height[left] >= leftMax)
+                leftMax = height[left];
+            else
+                water += leftMax - height[left];
+
+            left++;
+        }
+        else
+        {
+            if (height[right] >= rightMax)
+                rightMax = height[right];
+            else
+                water += rightMax - height[right];
+
+            right--;
+        }
+    }
+
+    return water;
+}
+
+int main()
+{
+    int height[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int n = 12;
+
+    printf("Trapped water = %d\n", trap(height, n));
+
+    return 0;
+}
